9c4ecc430ab701555a6b1d81a96fc4a90e999999
ahss8282 <ahss8282@gmail.com>
2016-04-03 18:36:34 +0900

    테스트로 문장을 추가해보았습니다.

--- app/src/main/java/org/androidtown/mouseproj2/MainActivity.java
+++ app/src/main/java/org/androidtown/mouseproj2/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("문장을 추가합니다.");
     }
 }
