5b8f2f6484b9504669e29176e3df86e8ed5db373
jaeshim <sjm767@naver.com>
2016-04-27 19:08:29 +0900

    코드정리는 나중에함

--- app/src/main/java/org/androidtown/mouseproj2/MainActivity.java
+++ app/src/main/java/org/androidtown/mouseproj2/MainActivity.java
@@ -1,14 +1,89 @@
 package org.androidtown.mouseproj2;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private long lastTime;
+    private float speed;
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+    private float x, y, z;
+
+    private static final int SHAKE_THRESHOLD = 800;
+    private static final int DATA_X = 0;
+    private static final int DATA_Y = 1;
+    private static final int DATA_Z = 2;
+
+    private SensorManager sensorManager;
+    private Sensor accelerormeterSensor;
+    private TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("문장을 추가합니다.");
+
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerormeterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        myText = (TextView) findViewById(R.id.myText);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (accelerormeterSensor != null)
+            sensorManager.registerListener(this, accelerormeterSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (sensorManager != null)
+            sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long currentTime = System.currentTimeMillis();
+            long gabOfTime = (currentTime - lastTime);
+            if (gabOfTime > 3000) {
+                lastTime = currentTime;
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2];
+
+                speed = Math.abs(x + y + z - lastX - lastY - lastZ) / gabOfTime * 10000;
+                //흔들림의 정도에따라 이벤트를 넣게됨
+                if (speed > SHAKE_THRESHOLD) {
+
+                }
+
+                lastX = event.values[DATA_X];
+                lastY = event.values[DATA_Y];
+                lastZ = event.values[DATA_Z];
+                myText.setText(String.valueOf(event.values[0])+","+String.valueOf(event.values[1])+","+String.valueOf(event.values[2]));
+            }
+
+        }
     }
 }
+
+
+
